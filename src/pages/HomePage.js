fbf6560b9fb8f64ffc75f24f9095a274c3dbba53
acep nurman sidik <acepnurmansidik@gmail.com>
2022-12-27 11:33:37 +0700

    section clients & sitemap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
+import React from "react";
+import Clients from "parts/Clients";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
-import React from "react";
+import Sitemap from "parts/Sitemap";
 
 export default function HomePage() {
   return (
@@ -11,6 +13,8 @@ export default function HomePage() {
       <Hero />
       <BrowseRoom />
       <JustArrived />
+      <Clients />
+      <Sitemap />
     </>
   );
 }
