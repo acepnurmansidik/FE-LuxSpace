90d296c1509f9b8221ca657a4aa8ed7fdd91f552
acep nurman sidik <acepnurmansidik@gmail.com>
2022-12-27 10:41:20 +0700

    section home page & hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import HomePage from "pages/HomePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <h1 className="text-6xl">
-      Hello world!
-    </h1>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
