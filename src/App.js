a6ad2a268abdd575f3c698aa80df196eaf1725d8
acep nurman sidik <acepnurmansidik@gmail.com>
2022-12-31 00:56:38 +0700

    added page congratulation and not found

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "pages/HomePage";
 import Details from "pages/Details";
 import Cart from "pages/Cart";
+import Congratulation from "pages/Congratulation";
+import NotFound from "pages/NotFound";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
         <Route exact path="/" element={<HomePage />} />
         <Route path="/categories/:idc" element={<Details />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/congratulation" element={<Congratulation />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
