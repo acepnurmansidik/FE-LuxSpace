7c4388985b01ab13c23cdf68ebad89f92be80f76
acep nurman sidik <acepnurmansidik@gmail.com>
2022-12-30 23:58:19 +0700

    added page cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "pages/HomePage";
 import Details from "pages/Details";
+import Cart from "pages/Cart";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route exact path="/" element={<HomePage />} />
         <Route path="/categories/:idc" element={<Details />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
