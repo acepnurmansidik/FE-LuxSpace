3964100050c060f4b0d62196af48a74f0602486f
acep nurman sidik <acepnurmansidik@gmail.com>
2023-01-12 06:14:56 +0700

    integration suggestion at detail page and added skelaton loader

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -27,11 +27,108 @@ export default function HomePage() {
           { url: "/categories/87211/products/", name: "Detail" },
         ]}
       />
-      <ProductDetail data={data?.detail} isLoading={isLoading} />
-      <Suggestion data={data?.relatedProducts} isLoading={isLoading} />
+      {isLoading ? (
+        <LoaderProductDetails />
+      ) : (
+        <ProductDetail data={data?.detail} />
+      )}
+
+      {isLoading ? (
+        <LoaderSuggestion />
+      ) : (
+        <Suggestion data={data?.relatedProducts} />
+      )}
+
       <Clients />
       <Sitemap />
       <Footer />
     </>
   );
 }
+
+const LoaderProductDetails = () => {
+  return (
+    <section className="container mx-auto">
+      <div className="flex flex-wrap my-4 md:my-12">
+        {/* for tablet and under */}
+        <div className="w-full md:hidden px-4">
+          <div className="w-80 h-16 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-40 h-4 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+        </div>
+
+        {/* for desktop */}
+        <div className="flex-1">
+          <div className="slider">
+            <div className="thumbnail">
+              {Array(5)
+                .fill()
+                .map((_, index) => (
+                  <div className="px-2 relative card group" key={index}>
+                    <div
+                      className="rounded-xl item bg-gray-300 animate-pulse"
+                      style={{ height: 106, width: 106 }}
+                    ></div>
+                  </div>
+                ))}
+            </div>
+            <div className="preview">
+              <div className="item rounded-lg h-full overflow-hidden">
+                <div className="item bg-gray-300 animate-pulse rounded-lg h-full overflow-hidden"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 px-4 md:p-6">
+          <div className="w-80 h-16 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-40 h-4 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+
+          <div className="w-44 h-10 mt-8 bg-gray-300 animate-pulse rounded-full"></div>
+          <hr className="my-8" />
+
+          <div className="w-36 h-6 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-88 h-6 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-60 h-6 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-88 h-6 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-60 h-6 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+          <div className="w-88 h-6 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const LoaderSuggestion = () => {
+  return (
+    <section className="bg-gray-100 px-4 py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-start mb-4">
+          <h3 className="text-2xl capitalize font-semibold">
+            Complete your room <br className="" />
+            with what we designed
+          </h3>
+        </div>
+        <div className="flex overflow-x-auto mb-4 -mx-3">
+          {Array(4)
+            .fill()
+            .map((_, index) => (
+              <div className="px-3 flex-none" style={{ width: 320 }}>
+                <div
+                  className="rounded-xl p-4 pb-8 relative bg-white"
+                  key={index}
+                >
+                  <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
+                    <div
+                      className="bg-gray-300 animate-pulse rounded-lg h-full"
+                      style={{ width: 287, height: 150 }}
+                    ></div>
+                  </div>
+                  <div className="w-56 h-4 mt-6 bg-gray-300 animate-pulse rounded-full"></div>
+                  <div className="w-40 h-4 mt-3 bg-gray-300 animate-pulse rounded-full"></div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/parts/Details/Suggestion.js
+++ src/parts/Details/Suggestion.js
@@ -1,6 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Suggestion() {
+export default function Suggestion({ data }) {
+  const data2 = [
+    {
+      id: 1,
+      idc: 2,
+      title: "Cangkir Mauttie",
+      price: 89300000,
+      imageUrl: "/images/content/chair-1.png",
+    },
+    {
+      id: 2,
+      idc: 2,
+      title: "Cangkir Mauttie",
+      price: 14500399,
+      imageUrl: "/images/content/chair-2.png",
+    },
+    {
+      id: 3,
+      idc: 2,
+      title: "Lino Dino",
+      price: 22000000,
+      imageUrl: "/images/content/chair-3.png",
+    },
+    {
+      id: 4,
+      idc: 2,
+      title: "Syail Ammeno",
+      price: 6399999,
+      imageUrl: "/images/content/chair-4.png",
+    },
+  ];
+
   return (
     <section className="bg-gray-100 px-4 py-16">
       <div className="container mx-auto">
@@ -11,70 +43,35 @@ export default function Suggestion() {
           </h3>
         </div>
         <div className="flex overflow-x-auto mb-4 -mx-3">
-          <div className="px-3 flex-none" style={{ width: 320 }}>
-            <div className="rounded-xl p-4 pb-8 relative bg-white">
-              <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                <img
-                  src="/images/content/chair-1.png"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
+          {data2?.map((item) => (
+            <div className="px-3 flex-none" style={{ width: 320 }}>
+              <div
+                className="rounded-xl p-4 pb-8 relative bg-white"
+                key={item.id}
+              >
+                <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.imageUrl}
+                    className="w-full h-full object-cover object-center"
+                  />
+                </div>
+                <h5 className="text-lg font-semibold mt-4">{item.title}</h5>
+                <span className="">
+                  {new Intl.NumberFormat("id", {
+                    style: "currency",
+                    currency: "IDR",
+                  }).format(item.price)}
+                </span>
+                <Link
+                  to={`/categories/${item.idc}/products/${item.id}`}
+                  className="stretched-link"
+                >
+                  {/* <!-- fake children --> */}
+                </Link>
               </div>
-              <h5 className="text-lg font-semibold mt-4">Cangkir Mauttie</h5>
-              <span className="">IDR 89.300.000</span>
-              <a href="details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </a>
             </div>
-          </div>
-          <div className="px-3 flex-none" style={{ width: 320 }}>
-            <div className="rounded-xl p-4 pb-8 relative bg-white">
-              <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                <img
-                  src="/images/content/chair-2.png"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Saman Kakka</h5>
-              <span className="">IDR 14.500.399</span>
-              <a href="details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </a>
-            </div>
-          </div>
-          <div className="px-3 flex-none" style={{ width: 320 }}>
-            <div className="rounded-xl p-4 pb-8 relative bg-white">
-              <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                <img
-                  src="/images/content/chair-3.png"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Lino Dino</h5>
-              <span className="">IDR 22.000.000</span>
-              <a href="details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </a>
-            </div>
-          </div>
-          <div className="px-3 flex-none" style={{ width: 320 }}>
-            <div className="rounded-xl p-4 pb-8 relative bg-white">
-              <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
-                <img
-                  src="/images/content/chair-1.png"
-                  alt=""
-                  className="w-full h-full object-cover object-center"
-                />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Syail Ammeno</h5>
-              <span className="">IDR 6.399.999</span>
-              <a href="details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
